4258fefe95183bcb0f92ab0fb7677973d31954f0
xXHoGodXx <142801872+xXHoGodXx@users.noreply.github.com>
2023-09-23 13:09:21 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+// let priceList = [30000, 2000, 11000, 33000, 2000, 100000,57000, 14000, 18000, 8000]
+
+// let from = prompt('цена от')
+// let up = prompt('цена до')
+
+
+// let callBack = ((priceList) => priceList === from >= up)
+
+// let find = priceList.filter(callBack)
+
+// if (priceList  >= from && priceList < up ) {
+//    alert(priceList)
+// }
+
+let students = ['baxtzod', 'xamza', 'islom', 'abdumanon', 'xojik', 'aziz']
+
+let callBack = ((students) => students.find_longest_word(str))
+
+ let find = priceList.filter(callBack)
